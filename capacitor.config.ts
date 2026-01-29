55e37c9ad903f04d8042cb625b26f2415519c94e
Johnson <johnsonjeyakumar004@gmail.com>
2026-01-29 13:46:54 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.leofitness.app',
+  appName: 'Naan Thanda Leo',
+  webDir: 'dist'
+};
+
+export default config;
